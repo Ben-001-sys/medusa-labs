39d88028b52e109ff31dc2ccbcb4c3d778d3d139
Hotor Benjamin <hotorben001@gmail.com>
2026-06-30 16:07:05 +0000

    notification

--- apps/backend/medusa-config.ts
+++ apps/backend/medusa-config.ts
@@ -12,7 +12,7 @@ module.exports = defineConfig({
   projectConfig: {
     workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server",
     databaseUrl: process.env.DATABASE_URL,
-    // redisUrl: process.env.REDIS_URL,
+    redisUrl: process.env.REDIS_URL,
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
@@ -24,38 +24,6 @@ module.exports = defineConfig({
 
   modules: [
     {
-      resolve: "./src/modules/brand"
-    }
-    // {
-    //   resolve: "@medusajs/medusa/caching",
-    //   options: {
-    //     providers: [
-    //       {
-    //         resolve: "@medusajs/caching-redis",
-    //         id: "caching-redis",
-    //         is_default: true,
-    //         options: {
-    //           redisUrl: process.env.REDIS_URL,
-    //         },
-    //       },
-    //     ],
-    //   },
-    // },
-    // {
-    //   resolve: "@medusajs/medusa/event-bus-redis",
-    //   options: {
-    //     redisUrl: process.env.REDIS_URL,
-    //   },
-    // },
-    // {
-    //   resolve: "@medusajs/medusa/workflow-engine-redis",
-    //   options: {
-    //     redis: {
-    //       redisUrl: process.env.REDIS_URL,
-    //     },
-    //   },
-    // },
-    ,{
       resolve: "@medusajs/medusa/locking",
       options: {
         providers: [
@@ -71,90 +39,17 @@ module.exports = defineConfig({
       },
     },
     {
+      resolve: "./src/modules/brand"
+    },
+// {
+//   resolve: "@medusajs/medusa/event-bus-redis",
+//   options: {
+//     redisUrl: process.env.EVENTS_REDIS_URL,
+//   },
+// },
+ {
       resolve: "./src/modules/delivery-slot",
     },
-    // {
-    //   resolve: "@medusajs/medusa/analytics",
-    //   options: {
-    //     providers: [
-    //       {
-    //         resolve: "@medusajs/analytics-posthog",
-    //         id: "posthog",
-    //         options: {
-    //           posthogEventsKey: process.env.POSTHOG_EVENTS_API_KEY,
-    //           posthogHost: process.env.POSTHOG_HOST,
-    //         },
-    //       },
-    //     ],
-    //   },
-    // },
-    // {
-    //   resolve: "@medusajs/medusa/file",
-    //   options: {
-    //     providers: [
-    //       {
-    //         resolve: "@medusajs/medusa/file-s3",
-    //         id: "s3",
-    //         options: {
-    //           file_url: process.env.S3_FILE_URL,
-    //           access_key_id: process.env.S3_ACCESS_KEY_ID,
-    //           secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
-    //           region: process.env.S3_REGION,
-    //           bucket: process.env.S3_BUCKET,
-    //           endpoint: process.env.S3_ENDPOINT,
-    //         },
-    //       },
-    //     ],
-    //   },
-    // },
-    // {
-    //   resolve: "@medusajs/medusa/notification",
-    //   options: {
-    //     providers: [
-    //       // ...
-    //       {
-    //         resolve: "@medusajs/medusa/notification-sendgrid",
-    //         id: "sendgrid",
-    //         options: {
-    //           channels: ["email"],
-    //           api_key: process.env.SENDGRID_API_KEY,
-    //           from: process.env.SENDGRID_FROM,
-    //         },
-    //       },
-    //     ],
-    //   },
-    // },
-    // {
-    //   resolve: "@medusajs/medusa/payment",
-    //   options: {
-    //     providers: [
-    //       {
-    //         resolve: "@medusajs/medusa/payment-stripe",
-    //         id: "stripe",
-    //         options: {
-    //           apiKey: process.env.STRIPE_API_KEY,
-    //         },
-    //       },
-    //     ],
-    //   },
-    // },
-    // {
-    //   resolve: "./src/modules/invoice-generator",
-    // },
-    // {
-    //   resolve: "./src/modules/product-review",
-    // },
-    // {
-    //   resolve: "./src/modules/brand",
-    // },
-    // {
-    //   resolve: "./modules/strapi",
-    //   options: {
-    //     apiUrl: process.env.STRAPI_API_URL,
-    //     apiToken: process.env.STRAPI_API_TOKEN,
-    //     defaultLocale: process.env.STRAPI_DEFAULT_LOCALE || "en",
-    //   },
-    // },
   ],
   // featureFlags: {
   //   caching: true,
